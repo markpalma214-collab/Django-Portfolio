a151e0c14fb2328c295625e7e3500d3e8f5af66a
Uploader Kun <markpalma214@gmail.com>
2026-09-12 22:07:29 +0800

    Switch database configuration to use dj_database_url

--- portfolio/portfolio/settings.py
+++ portfolio/portfolio/settings.py
@@ -8,7 +8,7 @@ list of variables you need to define in your own .env file.
 
 import os
 from pathlib import Path
-
+import dj_database_url
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -102,10 +102,10 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # migrations.
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
 
 
