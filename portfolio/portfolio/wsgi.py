8750e7c6e59e3f93b246b3d3568ac7d4fddac357
Uploader Kun <markpalma214@gmail.com>
2026-09-12 21:59:56 +0800

    Add base directory to WSGI configuration

--- portfolio/portfolio/wsgi.py
+++ portfolio/portfolio/wsgi.py
@@ -3,6 +3,11 @@ WSGI config for the portfolio project.
 """
 
 import os
+import sys
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+sys.path.insert(0, str(BASE_DIR))
 
 from django.core.wsgi import get_wsgi_application
 
